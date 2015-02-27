5bdd76d7b13a6715be14cdc6d43b6cc95d70dc3d
bag0paint <ch1619@txstate.edu>
2015-02-27 17:05:08 -0600

    MATLAB code change
    Changes to MATLAB code, untseted

--- MatlabCode1.m
+++ MatlabCode1.m
@@ -1,18 +1,22 @@
-% EExxxx Project 1
-% MATLAB code for xxxxxxxxxxxxx
-% Your Name and Last Name
-% Date
+% EE3370 Project 1
+% MATLAB code for function name
+% Your Charles Hinkle
+% 2/27/14
 
 clc      % Clear screen 
 clear    % Clear all variables
 A = 3; f= 1000; omega = 2*pi*f; 
 angle = degtorad(33); T=1/f;
 t=linspace(-3*T,3*T,1000);
-x_t=3*cos(omega*t+angle);
-subplot(3,1,1)
-plot(t,x_t, 'linewidth',2);grid on
-subplot(3,1,2)
 u_t=heaviside(t);
+u_t_1=heaviside(t-1);
+x_t=3*cos(omega*t+angle);
+f_t=cos(omega*t)*sin(omega*t)*u_t_1;
+subplot(4,1,1)
+plot(t,x_t, 'linewidth',2);grid on
+subplot(4,1,2)
 plot(t,u_t, 'linewidth',2);grid on
-subplot(3,1,3)
+subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
+subplot(4,1,4)
+plot(f_t, 'linewidth',2);grid on
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EE3370_Project_1_Hinkle_C
 #read me file
 #this is the change I made from my local machine
+#MATLAB code untseted and wasn't able to convert properly to SCILAB
