e5f097d0facca11bd7a212b3a92b3d6ac7003f2c
bag0paint <ch1619@txstate.edu>
2015-02-27 18:53:37 -0600

    MATLAB code
    Got MATLAB to work, I'm not sure that the plot is right.

--- MatlabCode1.m
+++ MatlabCode1.m
@@ -6,12 +6,12 @@
 clc      % Clear screen 
 clear    % Clear all variables
 A = 3; f= 1000; omega = 2*pi*f; 
-angle = degtorad(33); T=1/f;
+angle = (33*pi)/180; % degtorad didn't work on my MATLAB copy so changed it to formula.
+T=1/f;
 t=linspace(-3*T,3*T,1000);
 u_t=heaviside(t);
-u_t_1=heaviside(t-1);
 x_t=3*cos(omega*t+angle);
-f_t=cos(omega*t)*sin(omega*t)*u_t_1;
+f_t= cos(omega.*(t+1)) .* sin(omega.*(t+1)) .* u_t;
 subplot(4,1,1)
 plot(t,x_t, 'linewidth',2);grid on
 subplot(4,1,2)
@@ -19,4 +19,4 @@ plot(t,u_t, 'linewidth',2);grid on
 subplot(4,1,3)
 plot(t,x_t.*u_t, 'linewidth',2);grid on
 subplot(4,1,4)
-plot(f_t, 'linewidth',2);grid on
+plot((t-1),f_t, 'linewidth',2);grid on
